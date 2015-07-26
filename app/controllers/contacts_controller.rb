80b38e26e224fc5fd4d26d8fa3c915198f04b800
Bryan Coburn <fastturtlegraphics@outlook.com>
2015-07-26 04:39:26 +0000

    added contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,7 @@
+class ContactsCOntroller < ApplicationController
+    def new
+    end
+    
+    def create
+    end
+end
--- db/migrate/20150726042803_create_c_ontacts.rb
+++ db/migrate/20150726042803_create_c_ontacts.rb
@@ -1,6 +1,11 @@
 class CreateCOntacts < ActiveRecord::Migration
   def change
     create_table :c_ontacts do |t|
+      t.string :name
+      t.string :email
+      t.text :comments
+      
+      t.timestamps
     end
   end
 end
